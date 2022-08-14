9db357aa92430d22471a3c4c64d69094ff84d846
Oiching-SuiLin <13425535649@163.com>
2022-08-14 19:39:47 +0800

    test:owner without mapping

--- test/ownerManager.js
+++ test/ownerManager.js
@@ -6,59 +6,82 @@ contract("basewallet", function (accounts) {
     const owner_2 = accounts[2];
     const owner_3 = accounts[3];
     const owner_4 = accounts[4];
+    const owner_5 = accounts[5];
+    const owner_6 = accounts[6];
+    const owner_7 = accounts[7];
+    
     const module = accounts[5];
     let wallet_1;
+    let wallet_2
     let modules;
   
     before(async () => {
       modules = [module];
       wallet_1 = await BaseWallet.new();
+      wallet_2 = await BaseWallet.new();
       await wallet_1.init(owner_1, modules); 
+      await wallet_2.init(owner_5, modules)
     });
   
     describe("test ownerManager", () => {
       it("add owner", async () => {
         
-        await wallet_1.addOwner(wallet_1.address,owner_2);
-        let isOwner2 = await wallet_1.isOwner(wallet_1.address,owner_2)
-        console.log('isOwner1',isOwner2)
+        await wallet_1.addOwner(owner_2);
+        let isOwner21 = await wallet_1.isOwner(owner_2)
+        let isOwner22 = await wallet_2.isOwner(owner_2)
+        console.log('isOwner1',isOwner21,isOwner22)
         
-        await wallet_1.addOwner(wallet_1.address,owner_3);
-        let isOwner3 = await wallet_1.isOwner(wallet_1.address,owner_3)
-        console.log('isOwner1',isOwner3)
+        await wallet_1.addOwner(owner_3);
+        let isOwner31 = await wallet_1.isOwner(owner_3)
+        let isOwner32 = await wallet_2.isOwner(owner_3)
+        console.log('isOwner1',isOwner31,isOwner32)
 
-        let owners = await wallet_1.getOwners(wallet_1.address)
-        console.log('owners:')
+        let owners = await wallet_1.getOwners()
+        console.log('owners of wallet_1:')
         console.log(owners)
+
+        await wallet_2.addOwner(owner_6);
+        let isOwner61 = await wallet_1.isOwner(owner_6)
+        let isOwner62 = await wallet_2.isOwner(owner_6)
+        console.log('isOwner1',isOwner61,isOwner62)
+        
+        await wallet_2.addOwner(owner_7);
+        let isOwner71 = await wallet_1.isOwner(owner_7)
+        let isOwner72 = await wallet_2.isOwner(owner_7)
+        console.log('isOwner1',isOwner71,isOwner72)
+
+        let owners2 = await wallet_2.getOwners()
+        console.log('owners of wallet_2:')
+        console.log(owners2)
       });
       it("delete owner", async () => {
 
-        await wallet_1.deleteOwner(wallet_1.address,owner_2)
-        isOwner = await wallet_1.isOwner(wallet_1.address,owner_2)
+        await wallet_1.deleteOwner(owner_2)
+        isOwner = await wallet_1.isOwner(owner_2)
         console.log('isOwner1',isOwner)
-        owners = await wallet_1.getOwners(wallet_1.address)
+        owners = await wallet_1.getOwners()
         console.log(owners)
-        await wallet_1.deleteOwner(wallet_1.address,owner_3)
-        isOwner = await wallet_1.isOwner(wallet_1.address,owner_3)
+        await wallet_1.deleteOwner(owner_3)
+        isOwner = await wallet_1.isOwner(owner_3)
         console.log('isOwner1',isOwner)
-        owners = await wallet_1.getOwners(wallet_1.address)
+        owners = await wallet_1.getOwners()
         console.log(owners)
       })
       it("change owner", async () => {
         
-        await wallet_1.addOwner(wallet_1.address,owner_2);
-        await wallet_1.addOwner(wallet_1.address,owner_3);
-        let owners = await wallet_1.getOwners(wallet_1.address)
+        await wallet_1.addOwner(owner_2);
+        await wallet_1.addOwner(owner_3);
+        let owners = await wallet_1.getOwners()
         console.log('owners:')
         console.log(owners)
-        await wallet_1.changeOwner(wallet_1.address,owner_1,owner_4);
+        await wallet_1.changeOwner(owner_1,owner_4);
         
-        isOwner = await wallet_1.isOwner(wallet_1.address,owner_1)
+        isOwner = await wallet_1.isOwner(owner_1)
         console.log('isOwner1',isOwner)
         
-        isOwner = await wallet_1.isOwner(wallet_1.address,owner_4)
+        isOwner = await wallet_1.isOwner(owner_4)
         console.log('isOwner4',isOwner)
-        owners = await wallet_1.getOwners(wallet_1.address)
+        owners = await wallet_1.getOwners()
         console.log('owners:')
         console.log(owners)
       })
